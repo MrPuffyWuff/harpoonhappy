81144b50102facac3934f0b2c107bf48a4e76abe
MrPuffyWuff <jasper13.com@gmail.com>
2025-09-17 20:42:20 -0400

    Revert "WOrking ROPE"
    This reverts commit e781e7dc06b67edb63eacdae8b32c69c4167a580.

--- SCENES/PLAYER/WEAPON/ROPE/rope.gd
+++ SCENES/PLAYER/WEAPON/ROPE/rope.gd
@@ -3,79 +3,23 @@ extends Node3D
 class_name Rope
 
 @onready var path_3d := $Path3D
-@onready var path_follow := $Path3D/PathFollow3D
 
 var segments : int = 10
 var start : Vector3 = Vector3(0,0,0)
 var end : Vector3 = Vector3(0,-10,0)
-var start_anchor : Node3D
-var end_anchor : Node3D
-@export var final_rope_mass : float = 10
-@export var min_rope_mass : float = 6
+var start_anchor : NodePath
+var end_anchor : NodePath
 
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#Make path point to object
 	var rope_curve := Curve3D.new()
 	rope_curve.add_point(start)
 	rope_curve.add_point(end)
 	path_3d.curve = rope_curve
-	var rotation = Basis.looking_at(end - start)
-	#Generate Joints and Rigid bodies
-	var points = [start_anchor]
-	for i in range(0, 2*segments + 1):
-		var percent = i/float(2*segments)
-		path_follow.progress_ratio = percent
-		if i % 2 == 0:
-			var joint := Generic6DOFJoint3D.new()
-			joint.position = path_follow.position
-			joint.transform.basis = rotation
-			#joint.set_flag_x(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_LIMIT, true)
-			#joint.set_flag_y(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_LIMIT, false)
-			#joint.set_flag_z(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_LIMIT, true)
-			#joint.add_child(make_mesh())
-			points.append(joint)
-		else:
-			var body := make_rigid_body(rotation)
-			body.mass = lerp(min_rope_mass, final_rope_mass, percent)
-			#print(body.mass)
-			points.append(body)
-	points.append(end_anchor)
-	#Load in points
-	for i in range(1, len(points) - 1):
-		add_child(points[i])
-	for i in range(1, len(points) - 1):
-		if i % 2 == 1 and i < len(points) - 1:
-			var joint : Joint3D = points[i]
-			joint.node_a = points[i-1].get_path()
-			joint.node_b = points[i+1].get_path()
+	for i in range(0, 101, 100/(segments + 1)):
+		var percent = i/100.0
+		print(percent)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func make_rigid_body(rotation : Basis) -> RigidBody3D:
-	var body := RigidBody3D.new()
-	body.position = path_follow.position
-	body.transform.basis = rotation.inverse()
-	body.axis_lock_angular_y = true
-	body.mass = 10
-	#body.angular_damp = 1
-	#TEMP - Mesh for debugging sake
-	var mesh_instance : MeshInstance3D = make_mesh()
-	body.add_child(mesh_instance)
-	#The Collision Hit Box
-	var colliding_box := CollisionShape3D.new()
-	var collision_shape := CapsuleShape3D.new()
-	collision_shape.height = 1
-	collision_shape.radius = 0.25
-	colliding_box.shape = collision_shape
-	body.add_child(colliding_box)
-	return body
-
-func make_mesh() -> MeshInstance3D:
-	var mesh_instance := MeshInstance3D.new()
-	var mesh := CapsuleMesh.new()
-	mesh.height = 1
-	mesh.radial_segments = 8
-	mesh.radius = 0.25
-	mesh_instance.mesh = mesh
-	return mesh_instance
